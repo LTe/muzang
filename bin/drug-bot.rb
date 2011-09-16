e178837686984378d03a9c274d4b686c875b4b8a
Piotr Niełacny <piotr.nielacny@gmail.com>
2011-09-16 16:24:57 +0200

    Reddit plugin

--- bin/drug-bot.rb
+++ bin/drug-bot.rb
@@ -5,13 +5,15 @@ require 'drug-bot/plugins/plusone'
 require 'drug-bot/plugins/livereload'
 require 'drug-bot/plugins/motd'
 require 'drug-bot/plugins/rubygems'
+require 'drug-bot/plugins/reddit'
 
 EM.run do
   @bot = DrugBot::Bot.new
   @bot.register_plugin(PlusOne)
   @bot.register_plugin(LiveReload)
   @bot.register_plugin(Motd)
-  @bot.register_plugin(RubyGems)
+  #@bot.register_plugin(RubyGems)
+  @bot.register_plugin(Reddit)
   @bot.start
 end
 
---
+++ lib/drug-bot/plugins/reddit.rb
@@ -0,0 +1,43 @@
+require 'rss/1.0'
+require 'rss/2.0'
+require 'eventmachine'
+
+class Reddit
+  def initialize(bot)
+    @bot = bot
+
+    unless File.exist?(@config = ENV["HOME"] + "/.drug-bot")
+      FileUtils.mkdir @config
+    end
+
+    unless File.exist? @config + "/last_update.yml"
+      db = YAML::dump Time.now
+      File.open(@config + "/last_update.yml", "w"){|f| f.write(db)}
+    end
+
+    @last_update = YAML::load File.open(@config + "/last_update.yml", "r").read
+  end
+
+  def call(connection, message)
+    if message[:command] == "JOIN" && message[:nick] == connection.options[:nick]
+      EventMachine::add_periodic_timer(30) do
+        c_http = EM::Protocols::HttpClient2.connect 'www.reddit.com', 80
+        http = c_http.get "/r/ruby/.rss"
+
+        http.callback {|response|
+          rss = RSS::Parser.parse(response.content, false)
+          puts rss.items.size
+          rss.items.each do |item|
+            connection.msg(message[:channel], "#{item.title} | #{item.link}") if item.date > @last_update
+          end
+          save
+        }
+      end
+    end
+  end
+
+  def save
+    @last_update = Time.now
+    file = File.open(@config + "/last_update.yml", "w"){|f| f.write YAML::dump(@last_update)}
+  end
+end
