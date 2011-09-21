a225191bd48d57d041b0503ef0bfabaf50bef766
Jan Szumiec <jan.szumiec@gmail.com>
2011-09-21 19:05:48 +0800

    Eval odpala sie na osobnym watku, nie blokuje EM.

--- lib/drug-bot/plugins/eval.rb
+++ lib/drug-bot/plugins/eval.rb
@@ -4,18 +4,15 @@ class << Safe
      error = nil
 
      begin
-       thread = Thread.new {
-         $-w = nil
-
-         sandbox ||= Object.new.taint
-
-         yield(sandbox) if block_given?
-
-         $SAFE = 5
-         eval(code, sandbox.send(:binding))
-       }
-       value = thread.value
-       result = Marshal.load(Marshal.dump(thread.value))
+       $-w = nil
+       
+       sandbox ||= Object.new.taint
+       
+       yield(sandbox) if block_given?
+       
+       $SAFE = 5
+       value = eval(code, sandbox.send(:binding))
+       result = Marshal.load(Marshal.dump(value))
      rescue Exception => error
        error = Marshal.load(Marshal.dump(error))
      end
@@ -36,9 +33,15 @@ class Eval
   def call(connection, message)
     if message[:channel]
       if message[:message].match(/^\% (.*)/) && message[:message].match(/^\% (.*)/)[1]
-        result, error = safe(message[:message].match(/^\% (.*)/)[1])
-        connection.msg(message[:channel], "#{result}") if result
-        connection.msg(message[:channel], "Error: #{error}") if error
+        operation = proc do
+          safe(message[:message].match(/^\% (.*)/)[1])
+        end
+        callback = proc do |tuple|
+          result, error = tuple
+          connection.msg(message[:channel], "#{result}") if result
+          connection.msg(message[:channel], "Error: #{error}") if error
+        end
+        EM.defer(operation, callback)
       end
     end
   end
